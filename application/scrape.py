e98103d5a5c9b5da9bf6dbe7627fe4d202b0bc2b
runo hr <runoharris215@gmail.com>
2022-01-28 04:47:25 -0800

    Added docstrings

--- application/scrape.py
+++ application/scrape.py
@@ -6,6 +6,7 @@ from urllib.parse import urlparse
 
 class URL:
     def url_validator(self, url):
+        """Returns True if url is valid and False otherwise"""
         try:
             result = urlparse(url)
             return all([result.scheme, result.netloc, result.path])
@@ -24,6 +25,7 @@ class Scraper:
         self.most_freq = self.most_frequent() #dict
 
     def scrape_page(self, url):
+        """ Calls the clean_words method with a list of all words on a page"""
         words_list = []
         source_code = requests.get(url).text
         soup = BeautifulSoup(source_code, 'html.parser')
@@ -37,6 +39,10 @@ class Scraper:
         return self.clean_words(words_list)
 
     def clean_words(self, words_list):
+        """ Removes symbols from all words of the page
+            Appends all words with numbers to self.alphanum_words
+            Calls self.word_frequency with a list of cleaned words
+        """
         symbols = "!@#$%^&*()_-+={[}]|\;:\"<>?/., "
         clean_lst = []
         for word in words_list:
@@ -50,23 +56,29 @@ class Scraper:
         return self.word_frequency(clean_lst)
 
     def has_digit(self, word):
+        """Returns True if word has a number or is a number, else False"""
         # next() checking for each element, reaches end, if no element found as digit
         res = True if next((chr for chr in word if chr.isdigit()), None) else False
         return res
 
     def has_alphanum(self, word):
+        """ Returns True if word has noth letters and numbers, else False"""
         if any(chr.isalpha() for chr in word) and any(chr.isdigit() for chr in word):
             return True
         else:
             return False
 
     def word_frequency(self, clean_lst):
+        """Creates a dictionary of words and their count of occurrence
+           Creates a set of all words on the page
+        """
         for word in clean_lst:
             self.count_dict[word] = self.count_dict.get(word, 0) + 1
         self.sorted_count = sorted(self.count_dict.items(), key=lambda kv: kv[1])
         self.unique_words = set(self.count_dict.keys())
 
     def most_frequent(self, n=10):
+        """Returns a dictionary of the top ten most frequent words"""
         c = Counter(self.count_dict)
         top = c.most_common(n)
         return dict(top)
@@ -92,6 +104,9 @@ class Compare(Scraper):
         self.combinations_of_two = self.two_combinations(self.titles_n_sets)
 
     def common_in_all(self, titles_n_sets):
+        """Uses set intersection to find words present in all pages
+            Each set in titles_n_sets belongs to a page
+        """
         temp = titles_n_sets[0][1]
         i = 1
         while i < len(titles_n_sets):
@@ -100,6 +115,7 @@ class Compare(Scraper):
         return temp
 
     def only_in_one(self, titles_n_sets):
+        """Uses set difference to find words only present in the first page"""
         output = {}
         temp = titles_n_sets[0][1]
         i = 1
@@ -110,6 +126,7 @@ class Compare(Scraper):
         return output
 
     def two_combinations(self, titles_n_sets):
+        """Compares each page with every other using set intersection and difference operations"""
         all_combs = []
         for x,y in itertools.combinations(titles_n_sets, 2):
             combs = {}
